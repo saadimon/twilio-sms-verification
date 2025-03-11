e81dad2b0477da27990733ec22e9a8734b0c5de8
Saad Nadeem <saadnadeemghauri@gmail.com>
2025-03-11 20:47:46 +0500

    fix: logger-try2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./.env" });
+require('dotenv').config({path:__dirname+'/./../../.env'})
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, VERIFICATION_SID } = process.env;
 
