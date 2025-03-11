1ac21c40c81eb444ed43b954137684c221debfbf
Saad Nadeem <saadnadeemghauri@gmail.com>
2025-03-11 20:39:13 +0500

    fix: logging 2

--- index.js
+++ index.js
@@ -53,5 +53,10 @@ app.post("/api/create-verification-code", async (req, res) => {
 });
 
 app.listen(3000 || process.env.port, () => {
-  console.log("Server is running on port 3000");
+  logger.info("Server is running on port 3000");
+  logger.info("Environment: ", {
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+    VERIFICATION_SID,
+  });
 });
